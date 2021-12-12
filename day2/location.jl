c6e042374508eccbf3a3782e0acb19c6b12efa96
Matthew Miller <miller.aerospace@gmail.com>
2021-12-12 17:40:43 -0600

    Day 2 part 2 solution

--- day2/location.jl
+++ day2/location.jl
@@ -4,7 +4,6 @@
 
 #Part 1
 function location_estimate(input)
-    #create an Array offseting by the first integers (reduces the dim by 1)
     horizontal = 0
     depth = 0
     for i in 1:length(input[:,1])
@@ -23,3 +22,26 @@ function location_estimate(input)
 end
 
 # p1 = 1648020
+
+#Part 2
+function location_with_aim(input)
+    horizontal = 0
+    depth = 0
+    aim = 0
+    for i in 1:length(input[:,1])
+        if input[i,1] == "forward"
+            horizontal = horizontal + input[i,2]
+            depth = depth + aim*input[i,2]
+        elseif  input[i,1] == "down"
+            aim = aim + input[i,2]
+        else input[i,1] == "up"
+            aim = aim - input[i,2]
+        end
+    end
+    println(horizontal)
+    println(depth)
+    course = horizontal*depth;
+    println(course)
+end
+
+#p2 = 1759818555
