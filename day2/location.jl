f4fd66289198320429ede7c4241fe23aaeb3b43f
Matthew Miller <miller.aerospace@gmail.com>
2021-12-12 17:11:19 -0600

    Day 2 Part 1 solution

---
+++ day2/location.jl
@@ -0,0 +1,28 @@
+# Import data file
+# movement = readdlm("day2_input.txt", ' ')
+# input = readdlm("day2_example.txt", ' ')
+
+#Part 1
+function location_estimate(input)
+    #create an Array offseting by the first integers (reduces the dim by 1)
+    horizontal = 0
+    depth = 0
+    forward = "forward"
+    down = "down"
+    up = "up"
+    for i in 1:length(input[:,1])
+        if input[i,1] == "forward"
+            horizontal = horizontal + input[i,2]
+        elseif  input[i,1] == "down"
+            depth = depth + input[i,2]
+        else input[i,1] == "up"
+            depth = depth - input[i,2]
+        end
+    end
+    #println(horizontal)
+    #println(depth)
+    course = horizontal*depth;
+    println(course)
+end
+
+# p1 = 1648020
