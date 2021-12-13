baedf73c1813a60260ae436c68d1f8f70cd255f4
Matthew Miller <miller.aerospace@gmail.com>
2021-12-13 11:34:42 -0600

    Day 3 part2 solution

--- day3/binary_diagnostic.jl
+++ day3/binary_diagnostic.jl
@@ -14,7 +14,7 @@ function power_consumption(input)
     end
     #println(size(count))
     
-    #calculate gamma
+    #calculate gamma and epsilon
     gamma = zeros(Int8,1, length(input[1]))
     epsilon = zeros(Int8,1, length(input[1]))
     for i in 1:length(gamma)
@@ -29,8 +29,86 @@ function power_consumption(input)
     end
     #println(gamma)
     #println(epsilon)
+
+    #calculate power
     gamma_decimal = parse(Int, join(gamma),base=2)
     epsilon_decimal = parse(Int, join(epsilon),base=2)
     power = gamma_decimal*epsilon_decimal
     println(power)
 end
+
+#p1 solution = 3985686
+
+#Part 2
+function lift_support_rating(input)
+    #convert Vector{Strings} to a matrix of integers
+    count = zeros(Int8,(length(input), length(input[1])))
+    for i in 1:length(input)
+        temp = split(input[i],"")
+        for j in 1:length(temp)
+            count[i,j] = parse(Int, temp[j])
+        end        
+    end
+    #println(count)
+    #count
+    
+    #calculate oxygen
+    ox_count = count
+    index = length(ox_count[:,1])
+    while true
+        for i in 1:length(ox_count[1,:])
+            a = countmap(ox_count[:,i])
+            if a[0] > a[1]
+                ox_count = ox_count[(ox_count[:,i] .== 0),:]
+                index = length(ox_count[:,1])
+            elseif a[0] == a[1]
+                ox_count = ox_count[(ox_count[:,i] .== 1),:]
+                index = length(ox_count[:,1])
+            elseif a[0] < a[1]
+                ox_count = ox_count[(ox_count[:,i] .== 1),:]
+                index = length(ox_count[:,1])
+            end
+            #println(ox_count)
+        end
+        if index == 1
+            break
+        end
+    end
+    
+    #calculate co2
+    co2_count = count
+    index = length(co2_count[:,1])
+    while true
+        #global index = length(co2_count[:,1])
+        for i in 1:length(co2_count[1,:])
+            #println(index)
+            a = countmap(co2_count[:,i])
+            if index == 1
+                break
+            elseif a[0] < a[1]
+                co2_count = co2_count[(co2_count[:,i] .== 0),:]
+                index = length(co2_count[:,1])
+            elseif a[0] == a[1]
+                co2_count = co2_count[(co2_count[:,i] .== 0),:]
+                index = length(co2_count[:,1])
+            elseif a[0] > a[1]
+                co2_count = co2_count[(co2_count[:,i] .== 1),:]
+                index = length(co2_count[:,1])
+            end
+            #println(co2_count)
+        end
+        if index == 1
+            break
+        end
+    end
+    # println(ox_count)
+    # println(co2_count)
+    
+    #calculate life support rating
+    oxygen = parse(Int, join(ox_count),base=2)
+    co2 = parse(Int, join(co2_count),base=2)
+    rating = oxygen*co2
+    println(rating)
+end
+
+#p2 solution: 2555739
