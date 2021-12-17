d8a85e879d68d290685b2944b8710c45989400ef
Matthew Miller <miller.aerospace@gmail.com>
2021-12-17 14:29:38 -0600

    code commenting

--- day4/bingo_system.jl
+++ day4/bingo_system.jl
@@ -9,16 +9,16 @@ function sanitize_deck(input)
     draw_numbers = [parse(Int, ss) for ss in split(input[1],",")];
     
     #Isolate the bingo boards as a (5 x 5 x # of boards) matrix of integers
-    boards = zeros(Int8,5, 5,Int((length(input)-1)/6));
-    board = zeros(Int8,5,5);
+    boards = zeros(Int8,5, 5,Int((length(input)-1)/6)); #initialize a blank matrix to store each board
+    board = zeros(Int8,5,5); #initialize a blank single board for housing the vec(int) values
     n_boards = 1;
-    for i in 3:6:length(input)
-        for j in 1:5
-            board[:,j] = [parse(Int, ss) for ss in split(input[(j-1)+i])]
+    for i in 3:6:length(input) #iterate over the starting line of each board
+        for j in 1:5 #iterate over the rows within each board
+            board[:,j] = [parse(Int, ss) for ss in split(input[(j-1)+i])] #parse each line of strings to vec(int)
         end
-        boards[:,:,n_boards] = board'
-        board = zeros(Int8,5,5)
-        n_boards = n_boards + 1
+        boards[:,:,n_boards] = board' #input the vec(int) board into the full set of boards
+        board = zeros(Int8,5,5) #reinitialize the empty board for the next round
+        n_boards = n_boards + 1 #iterate on the next board
     end
 
     return (boards, draw_numbers)
